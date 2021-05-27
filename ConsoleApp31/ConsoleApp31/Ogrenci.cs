4de25d3e12e3303c88ced4df3691ddf000071047
Kalitrik <70946597+KALITRIK@users.noreply.github.com>
2021-05-27 23:06:30 +0300

    Class ve metodlar pratiki

---
+++ ConsoleApp31/ConsoleApp31/Ogrenci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    class Ogrenci
+    {
+
+        private int Öğrencino;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okul;
+
+        public Ogrenci(int _Öğrencino , string _isim , string _soyisim , int _vize1 , int _vize2 , int _final , string _okul )
+        {
+
+            Öğrencino = _Öğrencino;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okul = _okul;
+
+        }
+
+        public void Bilgi()
+        {
+
+            Console.WriteLine("Öğrencinin okul numarası: " + Öğrencino);
+            Console.WriteLine("Öğrencinin adı: " + isim + soyisim);
+            Console.WriteLine("Öğrencinin okulu: " + okul);
+
+        }
+
+        public void Not()
+        {
+            Console.WriteLine("(Üniversite not sistemini bilmiyorum ağlamayın )Öğrencinin not ortalaması: " + ((vize1+vize2+final) / 3));
+            Console.WriteLine("Öğrencinin 1. Vize Notu: " + vize1);
+            Console.WriteLine("Öğrencinin 2. Vize Notu: " + vize2);
+            Console.WriteLine("Öğrencinin Final Notu: " + final);
+
+        }
+
+        public void Okulöğren()
+        {
+            Console.WriteLine("Okulu: " + okul);
+
+        }
+
+    }
+}
---
+++ ConsoleApp31/ConsoleApp31/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Öğrenci numarasını gir: ");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.Write("İsminizi yazın: ");
+            string B = Console.ReadLine();
+
+            Console.Write("Soyisminizi yazın:");
+            string C = Console.ReadLine();
+
+            Console.Write("1. Vize notunuzu yazın: ");
+            int D = int.Parse(Console.ReadLine());
+
+            Console.Write("2. Vize notunuzu yazın: ");
+            int E = int.Parse(Console.ReadLine());
+
+            Console.Write("Final notunuzu yazın: ");
+            int F = int.Parse(Console.ReadLine());
+
+            Console.Write("Okulunuzu yazın: ");
+            string G = Console.ReadLine();
+
+            bool kontrol = true;
+            Ogrenci Öğrenci = new Ogrenci(A, B, C, D, E, F, G);
+            //int , string , string , int ,int , int , string
+
+            while (kontrol)
+            {
+
+                Console.WriteLine("1- Öğrenci Bilgilerini Göster");
+                Console.WriteLine("2- Öğrenci ortalamasını göster");
+                Console.WriteLine("3- Öğrencinin okulunu göster");
+                Console.WriteLine("4- Çıkış");
+
+                Console.Write("Hangi işlemi yapmak istiyorsunuz: ");
+
+                string if1 = Console.ReadLine();
+                if (if1 == "1")
+                {
+                    Console.WriteLine("");
+                    Öğrenci.Bilgi();
+                    Console.WriteLine("");
+                    Console.ReadLine();
+                }
+
+                else if (if1 == "2")
+                {
+                    Console.WriteLine("");
+                    Öğrenci.Not();
+                    Console.WriteLine("");
+                    Console.ReadLine();
+                }
+
+                else if (if1 == "3")
+                {
+                    Console.WriteLine("");
+                    Öğrenci.Okulöğren();
+                    Console.WriteLine("");
+                    Console.ReadLine();
+                }
+
+                else if (if1 == "4")
+                {
+                    kontrol = false;
+
+                }
+
+            }
+
+
+
+        }
+    }
+}
